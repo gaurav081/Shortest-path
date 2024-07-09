4fd52cd90ca611e91cfe06f1120500236e6c71b1
gaurav081 <gauravkr081@gmail.com>
2024-07-10 02:01:25 +0530

    all files added

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import PathfindingVisualizer from "./PathFindingVisualizer/PathFindingVisualizer";
+
+function App() {
+  return <PathfindingVisualizer />;
+}
+
+export default App;
